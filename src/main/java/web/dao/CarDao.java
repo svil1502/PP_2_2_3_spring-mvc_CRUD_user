8a17030075f7348251d92405a7a11d3ed87ef2a8
Svetlana Ilina <developersvil@gmail.com>
2023-04-30 15:32:41 +0300

    first

---
+++ src/main/java/web/dao/CarDao.java
@@ -0,0 +1,24 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.models.Car;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDao {
+    public static List<Car> cars;
+
+    private List<Car> carsNumber;
+
+    private static int CAR_COUNT;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(CAR_COUNT++, "mitcubici", 123));
+        cars.add(new Car(CAR_COUNT++, "nokia", 456));
+        cars.add(new Car(CAR_COUNT++, "volvo", 789));
+        cars.add(new Car(CAR_COUNT++, "pejo", 012));
+        cars.add(new Car(CAR_COUNT++, "volksvagen", 023));
+    }
+}
