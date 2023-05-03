43cc9c92bbf9ffe1cd8b2c2d2708cf41c4199e9a
Svetlana Ilina <developersvil@gmail.com>
2023-05-03 12:25:05 +0300

    repository

--- src/main/java/web/dao/UserDAOImp.java
+++ src/main/java/web/dao/UserDAOImp.java
@@ -2,13 +2,14 @@ package web.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import web.models.User;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDAOImp implements UserDAO {
     private final EntityManager entityManager;
 
